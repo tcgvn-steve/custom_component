bf1fa3039b2e8878efa4b9970eaac62150a512af
tcgvn-steve <steve@capogrecogroup.com>
2021-10-08 17:56:18 +0700

    Custom component

---
+++ src/lib/index.js
@@ -0,0 +1,7 @@
+/* eslint-disable import/prefer-default-export */
+import MyCustomComponent from './components/MyCustomComponent.react';
+import SteveComponent from './components/SteveComponent.react';
+
+export {
+    MyCustomComponent, SteveComponent
+};
